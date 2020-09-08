e748419fbe332ebc14d6ba210886a95e8f484159
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-09-08 16:41:53 -0400

    create cli.rb

--- lib/api.rb
+++ lib/api.rb
@@ -6,9 +6,8 @@ require_relative 'job'
 resp= RestClient.get("https://jobs.github.com/positions.json?description=&location=&page=3")
 jobs_arr=JSON.parse(resp.body, symbolize_names:true)
 job_instances=jobs_arr.collect do |job|
-  
-    Job.new(job)
+  Job.new(job)
 end
-# binding.pry
+binding.pry
 
 
