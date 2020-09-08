e2eb9c01b1e2c1aabad2603520185572c89a549f
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-09-08 18:17:54 -0400

    print_by_location method

--- lib/cli.rb
+++ lib/cli.rb
@@ -17,11 +17,29 @@ attr_accessor :all_locations
     end
 
     def print_locations
-        all_location.each.with_index(1) do |location,index|
+        all_locations.each.with_index(1) do |location,index|
             puts "#{index}. #{location}."
         end
         
     end
+
+
+def print_by_location (location)
+    Job.all.each do |job|
+
+                if job.location.downcase == location.downcase
+                    
+                        puts    "#{job.title}"
+                        puts    "This is a #{job.type} position "
+                        puts    "The name of the company is #{job.company} and the location will be #{job.location}"
+                        puts "You can learn about the company here : #{job.company_url}"
+                        puts "To know more about the position please click here: #{job.url}"
+                        puts "--------------------------------------------------------------------------------------------------------------------------------"
+                        puts "--------------------------------------------------------------------------------------------------------------------------------"    
+                end
+
+            end
+end
 
 
 end
