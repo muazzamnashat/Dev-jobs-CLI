2b22001d0ee1220c58715b774308dac3f143ec7d
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-09-09 14:52:17 -0400

    create print_languages and use .capitalize in all_lcations method.

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,14 +3,14 @@ require_relative 'api'
 require 'pry'
 
 class CLI
-attr_accessor :all_locations
+attr_accessor :all_location, :all_language
 
     def all_locations
         @all_location=[]
         API.get_list
         Job.all.each do |job|
            
-            @all_location << job.location
+            @all_location << job.location.capitalize
            
         end
         @all_location.uniq
@@ -24,23 +24,45 @@ attr_accessor :all_locations
     end
 
 
-def print_by_location (location)
+    def all_languages
+        @all_language=[]
+        API.get_list
+        Job.all.each do |job|
+        
+        if job.description.include?("Python")
+            @all_language << "Python"
+        elsif job.description.include?("Javascript")
+            @all_language << "Javascript"
+        elsif job.description.include?("Java")
+            @all_language << "Java"
+        elsif job.description.include?("C++")
+            @all_language << "C++"
+        elsif job.description.include?("C#")
+            @all_language << "C#"
+        elsif job.description.include?("C")
+            @all_language << "C"
+        elsif job.description.include?("Ruby")
+            @all_language << "Ruby"
+        end
+        end
+        @all_language.uniq
+    
+    end
+
+    def print_languages
+        all_languages.each.with_index(1) do |lan,index|
+            puts "#{index}. #{lan}."
+        end   
+    end
+
+def print_by_location_and_language(location,language)
     Job.all.each do |job|
-
-                if job.location.downcase == location.downcase
-                    
-                        puts    "#{job.title}"
-                        puts    "This is a #{job.type} position "
-                        puts    "The name of the company is #{job.company} and the location will be #{job.location}"
-                        puts "You can learn about the company here : #{job.company_url}"
-                        puts "To know more about the position please click here: #{job.url}"
-                        puts "--------------------------------------------------------------------------------------------------------------------------------"
-                        puts "--------------------------------------------------------------------------------------------------------------------------------"    
-                end
-
-            end
+        if job.location == location && job.description.include?(language)
+        puts "The location is #{job.location}"
+        end
+    end
+    
 end
-
 
 end
 binding.pry
