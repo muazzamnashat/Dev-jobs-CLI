a39306f5782b2904e2e8e2ef87e9b578f173305f
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-09-14 14:35:39 -0400

    refactor api class

--- lib/api.rb
+++ lib/api.rb
@@ -2,8 +2,8 @@ class API
     def self.get_list
         resp = RestClient.get("https://jobs.github.com/positions.json?description=&location=&page=3")
         jobs_arr = JSON.parse(resp.body, symbolize_names:true)
-        job_instances = jobs_arr.collect do |job|
-        Job.new(job)
+        jobs_arr.each do |job|
+            Job.new(job)
         end
     end
 end
